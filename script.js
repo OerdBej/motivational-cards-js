0129596c4d5806eec0b9e7142eaadecd0d88b7ec
Oerd Bej <oerdbej@gmail.com>
2023-04-13 18:54:30 +0200

    adding the contional to toggle the pictures to ON/OF when user clicked on it

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
-    removeClasses();
     panel.addEventListener('click', () => {
-        panel.classList.add('active');
+        // If the clicked panel already has the "active" class, remove it
+        if (panel.classList.contains('active')) {
+            panel.classList.remove('active');
+        } else {
+            // Otherwise, remove the "active" class from all other panels and add it to the clicked panel
+            removeClasses();
+            panel.classList.add('active');
+        }
     });
 });
 
-//delete active for each elements
-
+// Function to remove the "active" class from all panels
 function removeClasses() {
     panels.forEach((panel) => {
         panel.classList.remove('active');
